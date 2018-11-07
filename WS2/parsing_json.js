97f68b8b682247fa7f3d711a7ba773cc562dccff
mjstenbe <mika.stenberg@laurea.fi>
2018-11-07 11:51:07 +0200

    add

---
+++ WS2/parsing_json.js
@@ -0,0 +1,10 @@
+// Simple demo on parsing JSON
+
+var data = require("./quotes2.json");
+
+// Parse the results
+
+for (var i = 0; i < data.length; i++) {
+  console.log(data[i].quote);
+  console.log(data[i].author);
+}
